909dc25f1b3e6f13666b46db9569f4afd14206d8
Kristine Martin <kristinenicolemartin@gmail.com>
2018-01-06 22:31:26 -0800

    finished adding flux pattern, integrated mongodb, and created an api endpoint

--- js/components/Main.js
+++ js/components/Main.js
@@ -1,10 +1,29 @@
 import React, {Component} from 'react';
 import API from "../API";
-import LinkStore from "../stores/LinkStore";
+import LinkStore from "../stores/LinkStore.js";
+
+let _getAppState = () => {
+    return { links: LinkStore.getAll() };
+}
 
 export default class Main extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = _getAppState();
+        this.onChange = this.onChange.bind(this);
+    }
+
     componentDidMount() {
         API.fetchLinks();
+        LinkStore.on("change", this.onChange);
+    }
+    componentWillUnmount() {
+        LinkStore.removeListener("change", this.onChange);
+    }
+    onChange() {
+        console.log("4. In the View");
+        this.setState(_getAppState());
     }
     render() {
         return (
