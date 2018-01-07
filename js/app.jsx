fe4e08b6d7dfa9a5ab14d0b91729153a53027b25
Kristine Martin <kristinenicolemartin@gmail.com>
2018-01-06 22:12:01 -0800

    added serverActions, LinkStore, API, and AppDispatcher files for Flux

--- js/app.jsx
+++ js/app.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import Hello from './components/Hello';
+import Main from './components/Main';
 
-ReactDOM.render(<Hello />, document.getElementById('react'));
+ReactDOM.render(<Main />, document.getElementById('react'));
---
+++ js/stores/LinkStore.js
@@ -0,0 +1,8 @@
+import AppDispatcher from "../AppDispatcher";
+import {ActionTypes} from "../Constants";
+
+class LinkStore {
+
+}
+
+export default new LinkStore();
